604f580f5fbf51129ce9d10ada64a28f4290f01f
Manuel Mora Gordillo <manuito@gmail.com>
2026-08-25 10:06:00 +0200

    New version

--- puppet/recoverpass_greeter/manifests/init.pp
+++ puppet/recoverpass_greeter/manifests/init.pp
@@ -40,7 +40,7 @@
 
 class recoverpass_greeter (
   # Versión del paquete que hay en files/
-  String  $version           = '0.0.2',
+  String  $version           = '0.0.3',
 
   # Distribuir /etc/recoverpass/recoverpass.conf desde este módulo
   Boolean $gestionar_config  = true,
